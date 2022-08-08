f5e22a264bd15d94970196c648d2594124cd2ac1
Ghiloar <82312652+Ghiloar@users.noreply.github.com>
2022-08-08 15:19:50 +0200

    Update cake_problem.py

--- cake-candles/cake_problem.py
+++ cake-candles/cake_problem.py
@@ -1,16 +1,24 @@
+# For info see https://www.youtube.com/watch?v=FkVe8qrT0LA
 from random import random
 from itertools import count
-prob = 0
-for i in count():
-    c = random()
-    cp = random()
-    k = random()
-    if c > cp:
-        a = c
-        c = cp
-        cp = a
-    if c < k and k < cp:
-        prob += 1
-    if i%10000000 == 0: 
-        prob2 = prob/(i+1)
-        print(prob2, i)
+
+# Probability of having a candle on each section of the cake
+probability = 0
+
+for i in count(1):
+    candle_1 = random()
+    candle_2 = random()
+    knife = random()
+    
+    # The program assumes that candle_1 is smaller than
+    # candle_2 so it swaps them if this is not the case
+    if candle_1>candle_2:
+        a = candle_1
+        candle_1 = candle_2
+        candle_2 = a
+        
+    if candle_1<knife and knife<candle_2:
+        probability += 1
+        
+    if i%10**7==0: 
+        print(probability/i, i)
